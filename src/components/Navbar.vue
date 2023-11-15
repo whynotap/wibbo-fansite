1a2a89105d2f4f937ff7988e9bcf85c011ea7ec0
worldsofk3 <qdmk3ntin@gmail.com>
2023-11-15 22:35:08 +0100

    header, footer, index
    deux composants (header et footer) et une page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+		<div class="flex">
+			<img src="@/assets/imgs/icon_wb.gif" />
+
+			<img src="@/assets/imgs/default/back.jpg" />
+
+			<button type="button"><img src="@/assets/imgs/theatre_lights_icon.png" />Administration</button>
+		</div>
+		<nav>
+			<ul class="flex justify-center">
+				<li>
+					<a href="#"><img src="@/assets/imgs/user.png" /><span>Home</span></a>
+				</li>
+				<div>
+					<!-- Déroulant, à tester avec cette structure -->
+					<li>
+						<img src="@/assets/imgs/commu.png" /><span>Communauté</span>
+					</li>
+					<ul class="hidden">
+						<li>Articles</li>
+						<li>Équipe</li>
+					</ul>
+				</div>
+				<li>
+					<a href="#"><img src="@/assets/imgs/topme.gif" /><span>Sécurité</span></a>
+				</li>
+				<li>
+					<a href="#"><img src="@/assets/imgs/support.gif" /><span>Support</span></a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,62 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <Navbar />
+        <section class="grid grid-cols-2 gap-2 sm:w-3/4 mx-auto">
+            <div>
+                <img src="https://imaging.wibbo.org/?figure=hr-170-31.hd-209-1359.lg-280-110.sh-3035-110.ch-3030-110.he-3858-110&head_direction=3&gesture=sml" />
+                <div>
+                    <p>Python</p>
+                    <p>
+                        <img src="@/assets/imgs/mottohome.gif" />Python vous souhaite la
+                        bienvenue sur notre fansite officiel
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex justify-end">
+                <img src="@/assets/imgs/connexion.gif" />
+                <form class="flex flex-col">
+                    <label for="username">Espace Staff</label>
+                    <input
+                        id="username"
+                        type="text"
+                        placeholder="Ton pseudo Wibbo..."
+                    />
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Ton mot de passe..."
+                    />
+                </form>
+            </div>
+
+            <div class="col-span-2">
+                <div>
+                    <h4>Version bêta</h4>
+                    <p>
+                        Notre version bêta débarque et reste temporaire le temps du
+                        développement.
+                    </p>
+                    <a href="#"><button type="button">Lire la suite »</button></a>
+                </div>
+
+                <div>
+                    <h4>Bienvenue sur Wibbo'Up !</h4>
+                    <p>Et oui, enfin..</p>
+                    <a href="#"><button type="button">Lire la suite »</button></a>
+                </div>
+
+                <div>
+                    <h5>Discord</h5>
+                    <iframe
+                        src="https://discord.com/widget?id=1170468974992031856&theme=dark"
+                        width="350"
+                        height="200"
+                        allowtransparency="true"
+                        frameborder="0"
+                        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                    ></iframe>
+                </div>
+            </div>
+        </section>
+    <Footer />
 </template>
